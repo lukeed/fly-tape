31edecd57689972e6cd772c31f4fd7f4f46857a7
luke <luke.edwards05@gmail.com>
2016-03-06 15:40:28 -0800

    initial logic, testing

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var tape = require('tape');
 var assign = require('object-assign');
 
 module.exports = function () {
-  this.tape = function (options) {
+  this.tape = function (opts) {
+    opts = assign({
+      stream: process.stdout,
+      reporter: require('tap-spec'),
+      options: {}
+    }, opts);
 
+    var instance = tape.createStream(opts.options);
+    instance.pipe(opts.reporter).pipe(opts.stream);
+
+    console.log(instance);
   };
 };
